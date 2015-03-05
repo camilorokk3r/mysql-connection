bf5c20293c9975bdfa1105c083fdd0de025f1664
Camilo Martinez <camilo@rokk3rlabs.com>
2015-03-05 10:47:32 -0500

    Adjust code to be module

---
+++ index.js
@@ -0,0 +1,121 @@
+var mysql = require('mysql');
+var Tunnel = require('tunnel-ssh');
+var q = require('q');
+
+module.exports = { 
+
+  MySQLConnection: function (serverConfig, tunnelConfig){
+
+    this.tunnelPort = 3306;          // can really be any free port used for tunneling
+
+    /**
+     * DB server configuration. Please note that due to the tunneling the server host
+     * is localhost and the server port is the tunneling port. It is because the tunneling
+     * creates a local port on localhost
+     */
+    this.dbServer =  serverConfig;
+
+    /**
+     * Default configuration for the SSH tunnel
+     */
+    this.tunnelConfig = tunnelConfig;
+
+    this.connection = null;
+
+    /**
+     * Initialise the mysql connection via the tunnel. Once it is created call back the caller
+     *
+     */
+    this.init = function () {
+        //
+        // SSH tunnel creation
+        //
+        var defer = q.defer();
+
+        var me = this;
+        //console.log(me);
+
+        me.tunnel = new Tunnel(this.tunnelConfig);
+        me.tunnel.connect(function (error) {
+            
+            if(error){
+                defer.reject(error);
+            }else{
+              console.log('Tunnel connected');  
+
+              me.connect().then(function(connection){
+                defer.resolve(connection);
+              }, 
+              function(connectionError){
+                console.log("connectionError:", connectionError);
+                defer.reject(connectionError);
+              });
+            }
+
+        });
+
+        return defer.promise;
+    };
+
+    /**
+     * Mysql connection error handling
+     *
+     * @param err
+     */
+    this.errorHandler = function (err) {
+
+        var me = this;
+        console.log("Got Error:", err);
+        //
+        // Check for lost connection and try to reconnect
+        //
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('MySQL connection lost. Reconnecting.');
+            me.connection = me.connect();
+        } else if (err.code === 'ECONNREFUSED') {
+            //
+            // If connection refused then keep trying to reconnect every 3 seconds
+            //
+            console.log('MySQL connection refused. Trying soon again. ' + err);
+            setTimeout(function () {
+                me.connect();
+            }, 3000);
+        }
+    };
+
+    /**
+     * Connect to the mysql server with retry in every 3 seconds if connection fails by any reason
+     *
+     * @param callback
+     * @returns {*} created mysql connection
+     */
+    this.connect = function () {
+        
+        var defer = q.defer();
+        var me = this;
+
+        // Create the mysql connection object
+
+        var connection = mysql.createConnection(me.dbServer);
+        
+        connection.on('error', me.errorHandler);
+        //
+        // Try connecting
+        //
+        connection.connect(function (err) {
+            console.log("Connected!");
+            if (err){
+              defer.reject(err);
+            }else{
+              this.connection = connection;
+
+              defer.resolve(connection);
+            }
+        });
+        
+        
+
+        return defer.promise;
+    }
+  }
+};
---
+++ tests/index.js
@@ -0,0 +1,55 @@
+var should = require('chai').should();
+var MySQLConnection = require('../index.js').MySQLConnection;
+var config = {};
+config['database'] =   {
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: 'decipher10',
+  database: 'fittingroom_dev',
+  insecureAuth: true, 
+  //debug:true
+};
+config['tunnel'] =   {
+  remoteHost: '127.0.0.1', // mysql server host
+  remotePort: 3306, // mysql server port
+  localPort: 3306, // a available local port
+  verbose: true, // dump information to stdout
+  disabled: false, //set this to true to disable tunnel (useful to keep architecture for local connections)
+  sshConfig: { //ssh2 configuration (https://github.com/mscdex/ssh2)
+      host: 'decipherseo.com',
+      port: 22,
+      username: 'camilo',
+      password: 'nuhyy4g1',
+      insecureAuth: true
+      //privateKey: require('fs').readFileSync('<pathToKeyFile>'),
+  }     //passphrase: 'verySecretString' // option see ssh2 config
+};
+
+var mysqlConnection = new MySQLConnection(config['database'], config['tunnel']);
+
+mysqlConnection.init().then(function(connection){
+  
+  console.log("Established a MySQL connection:", connection.threadId);
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log('The solution is: ', rows[0].solution);
+
+    connection.end(function(){
+      connection.destroy();
+
+    });
+    process.exit();
+    
+  });
+
+
+  
+}, 
+function(err){
+  
+  console.log("Error:", err);
+});
+
+
